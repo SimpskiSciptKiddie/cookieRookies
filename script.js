ce29ca30c72c1e12c3c47ab2abacf1cb473bcc9a
Recall12345 <Noor-sror@hotmail.com>
2026-09-11 14:29:32 +0200

    r

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const nameButtons = document.querySelectorAll(".name-button");
+
+nameButtons.forEach((nameButton) => {
+	nameButton.addEventListener("click", () => {
+		alert(`Du klickade på ${nameButton.dataset.name}!`);
+	});
+});
 
